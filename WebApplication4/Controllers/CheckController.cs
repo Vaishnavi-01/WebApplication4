171c5d639ea31edadc86d71a95a2fd43e11095d3
Vaishnavi-01 <vaishnavi.vaishnavishah.shah@gmail.com>
2017-06-14 17:02:42 +0530

    WebApp

---
+++ WebApplication4/Controllers/CheckController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication4.Controllers
+{
+    public class CheckController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            IList<string> lstNames = new List<string>();
+            lstNames.Add("abc");
+            lstNames.Add("def");
+            lstNames.Add("qwe");
+
+            return lstNames;
+        }
+    }
+}
